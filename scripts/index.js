01d2352261b7910f17dd4617c8cf205f55931372
Harsha-B-S <harshabharadhwaj04@gmail.com>
2024-09-04 18:47:38 +0530

    Myntra Functional Clone

---
+++ scripts/index.js
@@ -0,0 +1,64 @@
+let bagItems;
+onLoad();
+
+function onLoad() {
+  let bagItemsStr = localStorage.getItem("bagItems");
+  bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
+  displayItemsOnHomePage();
+  displayBagIcon();
+}
+
+function addToBag(itemId) {
+  bagItems.push(itemId);
+  localStorage.setItem("bagItems", JSON.stringify(bagItems));
+  displayBagIcon();
+}
+
+function displayBagIcon() {
+  let bagIconCountElement = document.querySelector(".bag-item-count");
+  if (bagItems.length > 0) {
+    bagIconCountElement.style.visibility = "visible";
+    bagIconCountElement.innerText = bagItems.length;
+  } else {
+    bagIconCountElement.style.visibility = "hidden";
+  }
+}
+
+function Ratings(stars, noOfReviews) {
+  if (stars >= 1) {
+    return `${stars} ⭐  | ${
+      noOfReviews < 1000 ? noOfReviews : noOfReviews / 1000 + "k"
+    }`;
+  } else {
+    return `<i>Buy! and be the first one to review</i>`;
+  }
+}
+
+function displayItemsOnHomePage() {
+  let itemsContainerElement = document.querySelector(".items-container");
+  if (!itemsContainerElement) {
+    return;
+  }
+  let innerHtml = "";
+  items.forEach((item) => {
+    innerHtml += `
+    <div class="item-container">
+    <img class="item-image" src="${item.image}" alt="item image">
+    <div class="rating">
+       ${Ratings(item.rating.stars, item.rating.count)}
+    </div>
+    <div class="company-name">${item.company}</div>
+    <div class="item-name">${item.item_name}</div>
+    <div class="price">
+        <span class="current-price">Rs ${item.current_price}</span>
+        <span class="original-price">Rs ${item.original_price}</span>
+        <span class="discount">(${item.discount_percentage}% OFF)</span>
+    </div>
+    <button class="btn-add-bag" onclick="addToBag(${
+      item.id
+    });">Add to Bag</button>
+    </div>`;
+  });
+
+  itemsContainerElement.innerHTML = innerHtml;
+}
